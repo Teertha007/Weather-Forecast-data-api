dfe74606730fca7e5afe5f8b3d6d58de0fc3f98d
teertha <teertha.sarker.3@gmail.com>
2025-04-20 01:20:42 +0600

    Enhance data fetching to support multiple days and filter by weather type

--- backend.py
+++ backend.py
@@ -5,11 +5,19 @@ import requests
 API_KEY = "1a59d955b1716443f880d4a44d77b2a4"
 
 
-def get_data(place,days=None,kind=None):
+def get_data(place,days=1,kind=None):
     url =f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     responses = requests.get(url)
     data = responses.json()
-    return data
+    filtered_data = data["list"]
+    forecast_days = days
+    num_values = 8* forecast_days
+    filtered_data = filtered_data[:num_values]
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 if __name__== "__main__":
-    print(get_data(place="Dhaka"))
+    print(get_data(place="Dhaka",days=5, kind="Temperature"))
